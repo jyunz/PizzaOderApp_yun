9c532ade9164f80f7135fc964cae7b824123f736
Roy <J.jyun0525@gmail.com>
2022-03-15 16:10:25 +0900

    메인화면 -> baseActivity 기반으로 동작하도록 설정

--- app/src/main/java/com/example/pizzaoderapp_yun/MainActivity.kt
+++ app/src/main/java/com/example/pizzaoderapp_yun/MainActivity.kt
@@ -3,9 +3,23 @@ package com.example.pizzaoderapp_yun
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() { 
+    
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    setUpEvents()
+    setValues()
+
+
+}
+    override fun setUpEvents() {
+//        버튼 클릭등의 이벤트 관련 코드 모음.
     }
+
+    override fun setValues() {
+//        화면에 데이터를 표시하기 위한 코드 모음
+    }
+
+   
 }
