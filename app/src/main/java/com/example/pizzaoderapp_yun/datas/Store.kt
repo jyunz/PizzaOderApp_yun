34aa2c9bc6d342492e2070c59079584e563da198
Roy <J.jyun0525@gmail.com>
2022-03-16 14:36:41 +0900

    List view에 아이템누르면 = > 해당가게 정보를 들고 화면 전환

--- app/src/main/java/com/example/pizzaoderapp_yun/datas/Store.kt
+++ app/src/main/java/com/example/pizzaoderapp_yun/datas/Store.kt
@@ -1,9 +1,11 @@
 package com.example.pizzaoderapp_yun.datas
 
+import java.io.Serializable
+
 class Store(
     val name: String,
     val phoneNum: String,
-    val logoURL : String) {
+    val logoURL : String) : Serializable {
 
 
 }
--- app/src/main/java/com/example/pizzaoderapp_yun/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/example/pizzaoderapp_yun/fragments/PizzaStoreListFragment.kt
@@ -1,11 +1,13 @@
 package com.example.pizzaoderapp_yun.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.pizzaoderapp_yun.R
+import com.example.pizzaoderapp_yun.ViewStoreDetailActivity
 import com.example.pizzaoderapp_yun.adapters.PizzaStoreAdapter
 import com.example.pizzaoderapp_yun.datas.Store
 import kotlinx.android.synthetic.main.fragment_pizza_store_list.*
@@ -40,7 +42,15 @@ class PizzaStoreListFragment : Fragment() {
 
         pizzaStoreListView.adapter = mPizzaStoreAdapter
 
-//        3월 16일
+        pizzaStoreListView.setOnItemClickListener { parent, view, position, id ->
+
+            val clickedStore = mPizzaStoreDataList[position]
+
+            val myIntent = Intent(requireContext(),ViewStoreDetailActivity::class.java)
+
+            myIntent.putExtra("storeData", clickedStore)
+            startActivity(myIntent)
+         }
 
     }
 }
