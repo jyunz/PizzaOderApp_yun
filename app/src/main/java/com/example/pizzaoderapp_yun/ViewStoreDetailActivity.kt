49cfe883c3943c42587efbf61ccc6aa0e647ea19
Roy <J.jyun0525@gmail.com>
2022-03-16 14:17:21 +0900

    가게 상세보기 화면 추가

---
+++ app/src/main/java/com/example/pizzaoderapp_yun/ViewStoreDetailActivity.kt
@@ -0,0 +1,21 @@
+package com.example.pizzaoderapp_yun
+
+import android.os.Bundle
+
+class ViewStoreDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+        setUpEvents()
+        setValues()
+
+    }
+    override fun setUpEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+
+}
