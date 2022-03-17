938dcff4885b470368f9b1d76e8cb3c97509352f
Roy <J.jyun0525@gmail.com>
2022-03-17 12:53:05 +0900

    피자가게 목록 어댑터 뼈대 작업

--- app/src/main/java/com/example/pizzaoderapp_yun/adapters/ChikenStoreAdapter.kt
+++ app/src/main/java/com/example/pizzaoderapp_yun/adapters/ChikenStoreAdapter.kt
@@ -16,6 +16,10 @@ class ChikenStoreAdapter(
     val resId : Int,
     val mList : List<Store>) : ArrayAdapter<Store>(mContext, resId, mList) {
 
+//     inflater : xml을 가지고 => 객체로 변환해주는 (화면에 뿌리도록 도와주는) 변수를 멤버 변수로 생성
+//      getview 함수를 오버라이딩 => 기본 모양이 아닌 우리가 원하는 모양이 뿌려지게 하려고 오버라이딩
+//    convertView변수를  => var tempRow에 옮겨 담아서 => null인 경우에는 새로 inflatr해서 담자.
+
     val inflater = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
@@ -24,6 +28,8 @@ class ChikenStoreAdapter(
             tempRow = inflater.inflate(R.layout.chiken_store_list_item, null)
         }
 
+//        tempRow가 절대 null이 아닌 상태로 만들어주는 과정
+
         val row = tempRow!!
 
         val data = mList[position]
@@ -31,9 +37,9 @@ class ChikenStoreAdapter(
         val chikenStoreNameTxt = row.findViewById<TextView>(R.id.chikenStoreNameTxt)
         val chikenLogoImg = row.findViewById<ImageView>(R.id.chikenLogoImg)
 
-        storeNameTxt.text = data.name
+        chikenStoreNameTxt.text = data.name
 
-        Glide.with(mContext).load(data.logoURL).into(logoImg)
+        Glide.with(mContext).load(data.logoURL).into(chikenLogoImg)
 
 
         return row
